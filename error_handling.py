e26fc27cb8893166217ddebe9972ffdca0d8c71e
Adnane Habib <adnanou.habibou@gmail.com>
2023-04-07 10:32:48 +0300

    Data set
    Data set needed

--- error_handling.py
+++ error_handling.py
@@ -7,14 +7,14 @@ Team A6
 """
 Week 1 Error handling
 1. Write a function that takes a list of integers as an argument, and returns the sum of the integers. 
-   Use a try-except block to catch any ValueError exceptions that may be raised when attempting to convert a string to an integer..
+   Use a try-except block to catch any ValueError exceptions that may be raised when attempting to convert a string to an integer.
 """
 print("Welcome to our magic calculator system.") #greeting message
 myList =[] #creating an empty list
 word = "start"#initiating a variable that stores the entries
 while word !="END":#setting a key word to stop accepting entries
     #prompting the user for entries
-    word = input("Enter the next number, or press END to end the input: ")
+    word = input("Enter the next number, or type END to end the input: ")
     #appending the entry to the list
     myList.append(word)
 myList.pop(-1) #popping the "END" keyword
@@ -31,6 +31,41 @@ except:
 #code exit message
 finally:
     print("Thank you for using our services. See you soon.")
+
+
+"""
+Week 1 Error handling
+2. Write a function that takes a filename as an argument, and attempts to open the file. 
+   Use a try-except block to catch any FileNotFoundError exceptions that may be raised when attempting to open the file. 
+   If the file is successfully opened, the function should return the contents of the file.
+"""
+
+print("Welcome to our magic file reading system.") #greeting message
+#Prompting user to enter file name
+myFile = input("Enter the file you wish to open: ")
+#print("You have enetered ",myFile) #for debugging purposes
+#Testing of the file can open
+try:
+    #opening the file for read
+    reader = open(myFile,'r')
+    print("Succesfully opened the file.\n\n")
+#Catching file not found error
+except FileNotFoundError as e:
+    print('File does not exist:', e)
+#Catching other type of errors
+except Exception as e:
+    print('An error occured: ', e)
+#Processing the file data
+else:    
+    # for loop to skim through the file
+    for line in reader:
+        #print(reader.readline())
+        print(line) #printing the line
+    reader.close()#closing the file
+#code exit message
+finally:
+    print("\n\nThank you for using our services. See you soon.")
+
 
 """
 End of file
