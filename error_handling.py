ee09519d566ae5d8c68d36bb8b30799610c2208c
Adnane Habib <adnanou.habibou@gmail.com>
2023-04-07 09:19:17 +0300

    Create error_handling.py
    Initial commit

---
+++ error_handling.py
@@ -0,0 +1,15 @@
+"""
+Brainnest Python Training
+April 2023
+Adnane Habib
+Team A6
+"""
+"""
+Week 1
+1. Write a program that prompts the user for a string and checks whether the string is a palindrome (i.e., the string reads the same forward and backward).
+"""
+
+
+"""
+End of file
+"""
